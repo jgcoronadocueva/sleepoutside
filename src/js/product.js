f89450a7eccec99deb740d84238bdeac92c2504c
benjaminjalebeau <benjamin.ja.lebeau@gmail.com>
2024-09-21 11:54:44 -0600

    AddProductToCart Checks if it is empty, if so initializes an array. New cart items are now pushed into initialized array.

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  var cartItems = getLocalStorage("so-cart");
+
+  if (!cartItems) {
+    cartItems = [];
+  }
+
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
