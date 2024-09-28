1ea2a2493cb0daf716fec4d223de63802aeb59ef
ijromerob <ijromerob@gmail.com>
2024-09-27 19:59:37 -0600

    change the script tag in html and updated the function amountChangeHandler to hide the superScript until the cart is >0

--- src/js/superScriptHandler.js
+++ src/js/superScriptHandler.js
@@ -4,6 +4,12 @@ import { getLocalStorage } from "./utils.mjs";
 
 export default function amountChangeHandler() {
   let storageArray = getLocalStorage("so-cart") || [];
+
+  if (storageArray.length > 0) {
+    let amountContainer = document.querySelector(".amount-container");
+    amountContainer.style.display = "flex";
+  }
+
   let superScript = document.querySelector(".total-amount");
   superScript.innerHTML = `${storageArray.length}`;
 }
