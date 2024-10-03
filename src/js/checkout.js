81dc500bb09f4d3a29ccf82daadaa08d9c8200c5
benjaminjalebeau <benjamin.ja.lebeau@gmail.com>
2024-10-02 19:42:21 -0600

    testing netlify

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -4,5 +4,10 @@ import ShoppingCart from "./ShoppingCart.mjs";
 //returns templates to index.html for main page
 loadHeaderFooter();
 
+//the instructors solution didn't inlcude the actual rendering of the checkout page, 
+//so It's hard to say what it should look like. For now, 
+//it's basically a copy and paste of the cart page.
 let cartContents = new ShoppingCart(".shopping-list");
 cartContents.renderCartContents();
+
+
