242043b6ae946d8ceb59157b9994a2435370069f
benjaminjalebeau <benjamin.ja.lebeau@gmail.com>
2024-10-04 16:27:57 -0600

    commented out fitler fucntion in productlist.mjs. It does not seem we need this currently, but may need it later.

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -25,14 +25,16 @@ export default class ProductListing {
   // init also changes the the content of the title to the data category
   async init() {
     const list = await this.dataSource.getData(this.category);
-    this.renderList(this.filterList(list));
+    this.renderList(list);
     document.querySelector(".title").innerHTML = this.category;
   }
   renderList(list) {
     renderListWithTemplate(productCardTemplate, this.listElement, list);
   }
-  // created this method that filters the tents that are not supposed to be displayed.
-
+  
+  /*
+  Dont currently have a need to use this, but may in the future.
+  filters the products that are not supposed to be displayed.
   filterList(list) {
     let newArray = list.filter(({ Id }) => {
       let result = Id !== "989CG" && Id !== "880RT";
@@ -40,5 +42,5 @@ export default class ProductListing {
     });
 
     return newArray;
-  }
+  }*/
 }
