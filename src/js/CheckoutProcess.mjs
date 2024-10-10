6988f6bd1a0746fe3891203be127b4dc299cff6c
jcampos-94 <jcampos.94@hotmail.com>
2024-10-10 17:48:24 -0500

    Order summary displays properly

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -6,6 +6,7 @@ export default class CheckoutProcess {
       this.outputSelector = outputSelector;
       this.list = [];
       this.itemTotal = 0;
+      this.subTotal = 0;
       this.shipping = 0;
       this.tax = 0;
       this.orderTotal = 0;
@@ -19,20 +20,36 @@ export default class CheckoutProcess {
   
     calculateItemSummary() {
       // calculate and display the total amount of the items in the cart, and the number of items.
-        let itemsAmount = this.list.length;
-        let display = document.querySelectorAll(this.outputSelector + " num-items");
-        display.innerHTML = itemsAmount;
+        this.list.forEach(item => {
+            this.itemTotal += item.quantity;
+            this.subTotal +=  item.FinalPrice;
+        });
+
+        let displayAmount = document.querySelector(this.outputSelector + " #num-items");
+        displayAmount.innerHTML = this.itemTotal;
+
+        let displaySubTotal = document.querySelector(this.outputSelector + " #cartTotal");
+        displaySubTotal.innerHTML = `$${this.subTotal.toFixed(2)}`;
     }
   
     calculateOrdertotal() {
-      // calculate the shipping and tax amounts. Then use them to along with the cart total to figure out the order total
-  
+      // calculate the shipping and tax amounts. Then use them along with the cart total to figure out the order total
+        this.shipping = 10 + (this.itemTotal - 1) * 2;
+        this.tax = this.subTotal * 0.06;
+        this.orderTotal = this.subTotal + this.shipping + this.tax;
       // display the totals.
       this.displayOrderTotals();
     }
   
     displayOrderTotals() {
       // once the totals are all calculated display them in the order summary page
-  
+      let displayShipping = document.querySelector(this.outputSelector + " #shipping");
+      displayShipping.innerHTML = `$${this.shipping.toFixed(2)}`;
+
+      let displayTax = document.querySelector(this.outputSelector + " #tax");
+      displayTax.innerHTML = `$${this.tax.toFixed(2)}`;
+
+      let displayTotal = document.querySelector(this.outputSelector + " #orderTotal");
+      displayTotal.innerHTML = `$${this.orderTotal.toFixed(2)}`;
     }
   }
--- src/js/checkout.js
+++ src/js/checkout.js
@@ -4,5 +4,10 @@ import CheckoutProcess from "./CheckoutProcess.mjs";
 // Renders header and footer
 loadHeaderFooter();
 
-//let myCheckout = new CheckoutProcess("so-cart", ".checkout-summary");
-//myCheckout.init();
+let myCheckout = new CheckoutProcess("so-cart", ".checkout-summary");
+myCheckout.init();
+
+// Show Totals after zip code
+document
+  .querySelector("#zip")
+  .addEventListener("blur", myCheckout.calculateOrdertotal.bind(myCheckout));
