a0d495195f53d5faa518a32eeb6e4713c9109e76
jcampos-94 <jcampos.94@hotmail.com>
2024-10-10 17:16:33 -0500

    attempt at rendering the items ammount

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,4 +1,8 @@
 import { loadHeaderFooter } from "./utils.mjs";
+import CheckoutProcess from "./CheckoutProcess.mjs";
 
 // Renders header and footer
 loadHeaderFooter();
+
+//let myCheckout = new CheckoutProcess("so-cart", ".checkout-summary");
+//myCheckout.init();
